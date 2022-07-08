c62f7e7ae93363f7424d4a09e53071e36a1761d4
Hao Nguyen <dev.haonh@gmail.com>
2022-07-08 13:31:03 +0700

    upload flash space video

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterView } from "vue-router";
+
+import SideBarVue from "./components/SideBar.vue";
+</script>
+
+<template>
+  <div class="flex">
+    <SideBarVue />
+    <div>
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style></style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,4 @@
+<script setup></script>
+<template>
+  <div class="w-[236px] min-h-screen shadow"></div>
+</template>
